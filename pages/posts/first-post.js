c3e4149d79397e589d373cb731f53a59c75d9ecf
Max Tabolin <MakisNakis@yandex.ru>
2020-12-04 15:35:43 +0300

    + creating 2 pages

---
+++ pages/posts/first-post.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+import utilStyles from '../../styles/utils.module.css'
+
+
+function FirstPost() {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <section className={utilStyles.headingMd}>
+                    <p>Bonjour! Je m`appelle Maxim Tabolin. Je suis un ingénieur junior. </p>
+                </section>
+            </Layout>
+        </>
+    )
+}
+
+export default FirstPost
