456fd08722116f49684c68a61475f97ae021e0cd
Max Tabolin <MakisNakis@yandex.ru>
2020-12-07 23:32:22 +0300

    + fixed bug with comment

--- layouts/SiteLayout.js
+++ layouts/SiteLayout.js
@@ -7,7 +7,7 @@ const SiteLayout = ({ children }) => {
                 <nav className="flex justify-around items-center">
                     <div className="flex justify-around items-center w-52 h-20" >
                         <div className="text-center rounded-md bg-gradient-to-r from-blue to-indigo text-white text-7xl font-redhat font-bold w-10 h-10">S</div>
-                        <div className="text-center font-redhat font-medium text-logoName text-8xl w-31 h-9" >Shopka</div> //TODO: w-31 не работает
+                        <div className="text-center font-redhat font-medium text-logoName text-8xl w-31 h-9" >Shopka</div> {/*TODO: w-31 не работает*/}
                     </div>
                     <div className="">Sell on Shopka</div>
                     <div className="">Register</div>
