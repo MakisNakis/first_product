99cc2a0231c52561fb03b304dac57f92e44d86c7
Max Tabolin <MakisNakis@yandex.ru>
2020-12-07 23:30:37 +0300

    + fixed bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-for        extend: {
+        extend: {
             textColor: {
                 'logoName': '#0C2146'
             },
